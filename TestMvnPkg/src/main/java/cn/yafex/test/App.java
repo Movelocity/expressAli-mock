1952413b2d637c68000e7c7b3a859f1c582ea74a
HeWeikang <hollwaycond@qq.com>
2025-04-03 11:20:02 +0800

    在 TestMvnPkg 增加对模拟 sdk 的 使用

--- TestMvnPkg/src/main/java/cn/yafex/test/App.java
+++ TestMvnPkg/src/main/java/cn/yafex/test/App.java
@@ -1,5 +1,6 @@
 package cn.yafex.test;
-
+import com.expressAli.sdk.ShareService;
+import com.expressAli.sdk.ShareServiceImpl;
 // import global.iop.api.MockIopClient;
 /**
  * Hello world!
@@ -9,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ShareService shareService = new ShareServiceImpl();
+        System.out.println(shareService.shareContent("123"));
     }
 }
